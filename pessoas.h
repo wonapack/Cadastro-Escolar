bf999488e758b22f8074a9499dfc9ea9296da00a
Onerb2K <55460402+Onerb2K@users.noreply.github.com>
2019-11-04 22:21:33 -0300

    Update pessoas.h

---
+++ pessoas.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "estruturas.h"
+
+void cadastrar(cadastros pessoa[], int *qtda, int *qtda_al, int *qtda_prof);
+
+void removerpessoa(cadastros pessoa[], int *qtda);
+
+void exibirdados(cadastros pessoa[], int *qtda);
+
+void alterarDados(cadastros pessoa[], int *qtda);
+
+void exibe_pessoax(cadastros pessoa[], int *qtda);
+
+void help();
+
